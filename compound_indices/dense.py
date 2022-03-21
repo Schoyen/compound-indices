4c5fdb04f71d47729cf16a8dd61daff1dd08f61b
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2022-03-21 15:55:42 +0100

    Add asymmetric indexing for dense
      This is a case where it makes sense to have different axis lengths for
      the different indices.

--- compound_indices/dense.py
+++ compound_indices/dense.py
@@ -1,3 +1,6 @@
+import math
+
+
 def inds_to_comp(indices, L):
     N = len(indices)
 
@@ -16,5 +19,30 @@ def comp_to_inds(I, L, N):
         indices[i] = (I // (L ** (N - i - 1))) % L
 
     indices[-1] = I % L
+
+    return indices
+
+
+def inds_to_comp_as(indices, L):
+    N = len(indices)
+    assert len(L) == N
+
+    comp = 0
+
+    for i in range(N):
+        val = indices[i] * math.prod(L[i + 1 :])
+        comp += val
+
+    return comp
+
+
+def comp_to_inds_as(I, L, N):
+    indices = [0] * N
+    assert len(L) == N
+
+    for i in range(N - 1):
+        indices[i] = (I // math.prod(L[i + 1 :])) % L[i]
+
+    indices[-1] = I % L[-1]
 
     return indices
--- tests/test_dense.py
+++ tests/test_dense.py
@@ -1,4 +1,39 @@
-from compound_indices.dense import inds_to_comp, comp_to_inds
+import math
+
+from compound_indices.dense import (
+    inds_to_comp,
+    comp_to_inds,
+    inds_to_comp_as,
+    comp_to_inds_as,
+)
+
+
+def test_single_sq():
+    L = 5
+    counter = 0
+
+    for i in range(L):
+        comp = inds_to_comp((i,), L)
+        assert comp == counter
+        inds = comp_to_inds(comp, L, 1)
+        assert inds == [i]
+        counter += 1
+
+    assert counter == L
+
+
+def test_single_as():
+    L = [5]
+    counter = 0
+
+    for i in range(L[0]):
+        comp = inds_to_comp_as((i,), L)
+        assert comp == counter
+        inds = comp_to_inds_as(comp, L, 1)
+        assert inds == [i]
+        counter += 1
+
+    assert counter == math.prod(L)
 
 
 def test_pair_sq():
@@ -14,6 +49,21 @@ def test_pair_sq():
             counter += 1
 
     assert counter == L**2
+
+
+def test_pair_as():
+    L = [5, 6]
+    counter = 0
+
+    for i in range(L[0]):
+        for j in range(L[1]):
+            comp = inds_to_comp_as((i, j), L)
+            assert comp == counter
+            inds = comp_to_inds_as(comp, L, 2)
+            assert inds == [i, j]
+            counter += 1
+
+    assert counter == math.prod(L)
 
 
 def test_triple_sq():
@@ -32,6 +82,22 @@ def test_triple_sq():
     assert counter == L**3
 
 
+def test_triple_as():
+    L = [6, 3, 7]
+    counter = 0
+
+    for i in range(L[0]):
+        for j in range(L[1]):
+            for k in range(L[2]):
+                comp = inds_to_comp_as((i, j, k), L)
+                assert comp == counter
+                inds = comp_to_inds_as(comp, L, 3)
+                assert inds == [i, j, k]
+                counter += 1
+
+    assert counter == math.prod(L)
+
+
 def test_quad_sq():
     L = 7
     counter = 0
@@ -47,3 +113,20 @@ def test_quad_sq():
                     counter += 1
 
     assert counter == L**4
+
+
+def test_quad_sq():
+    L = [7, 7, 10, 3]
+    counter = 0
+
+    for i in range(L[0]):
+        for j in range(L[1]):
+            for k in range(L[2]):
+                for l in range(L[3]):
+                    comp = inds_to_comp_as((i, j, k, l), L)
+                    assert comp == counter
+                    inds = comp_to_inds_as(comp, L, 4)
+                    assert inds == [i, j, k, l]
+                    counter += 1
+
+    assert counter == math.prod(L)
